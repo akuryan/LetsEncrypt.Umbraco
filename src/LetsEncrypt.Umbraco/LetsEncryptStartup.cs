aa77805c850cb8affd3b19a2b115b0e7ddc2c882
Anton Kuryan <a.kuryan@colours.nl>
2016-09-27 13:32:32 +0300

    Added code to allow getting static files from .well-known folder

---
+++ src/LetsEncrypt.Umbraco/LetsEncryptStartup.cs
@@ -0,0 +1,29 @@
+﻿namespace LetsEncrypt.Umbraco
+{
+    using global::Umbraco.Web;
+
+    using Microsoft.Owin;
+    using Microsoft.Owin.FileSystems;
+
+    using Owin;
+
+    public class LetsEncryptStartup : UmbracoDefaultOwinStartup
+    {
+        public override void Configuration(IAppBuilder app)
+        {
+            app.Map("/.well-known", letsEncrypt =>
+            {
+                letsEncrypt.Use((context, next) =>
+                {
+                    IFileInfo file;
+
+                    var fileSystem = new PhysicalFileSystem(@".\.well-known");
+
+                    return !fileSystem.TryGetFileInfo(context.Request.Path.Value, out file) ? next() : context.Response.SendFileAsync(file.PhysicalPath);
+                });
+            });
+
+            base.Configuration(app);
+        }
+    }
+}
